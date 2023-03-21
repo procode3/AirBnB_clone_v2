6bef2c62d7aa1977fccb6bc5492999e6136403d8
Ubuntu <ubuntu@ip-172-31-24-132.ec2.internal>
2023-03-21 10:20:43 +0000

    fixed delete method in fs engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls = None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        else:
+            fd = {key: value for key, value in FileStorage.__objects.items() if cls.__name__ in key}
+            return fd
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +28,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        if obj:
+            key = f'{obj.__class__.__name__}.{obj.id}'
+            del FileStorage.__objects[key]
+            self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
