139a8b0d1b95c576b8c1d63b6f7c671c3ba20cb6
purity chege <111085086+Khaikhai01@users.noreply.github.com>
2023-03-21 14:30:55 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,6 +24,13 @@ class FileStorage:
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
+    def delete(self, obj=None):
+        """ deletes an object from storage dictionary """
+        if (obj):
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            del FileStorage.__objects[key]
+            self.save()
+
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
@@ -32,12 +39,6 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-
-    def delete(self, obj=None):
-        if obj:
-            key = f'{obj.__class__.__name__}.{obj.id}'
-            del FileStorage.__objects[key]
-            self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
