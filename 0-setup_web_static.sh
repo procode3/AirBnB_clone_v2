2ee013bb0834a592e34f4cb97137d85ca2eeb38e
procode3 <wr.t8r1@gmail.com>
2023-04-05 23:42:43 +0300

    Deploying web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Installing and configuring ngnic for Airbnb webstatic
+
+#installing nginx
+sudo apt-get update
+sudo apt-get install nginx -y
+
+#creating respositories if they dont exisit
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+#test html file
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+#symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#changing directory name
+sudo chown -R ubuntu:ubuntu /data/
+
+#updating root directory
+sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+
+#restarting server
+sudo service nginx reload
+sudo service nginx restart
