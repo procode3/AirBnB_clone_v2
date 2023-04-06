a5f34c8dd57aef7af8c49e9b9f654efc53e6747a
procode3 <wr.t8r1@gmail.com>
2023-04-06 11:39:50 +0300

    Creating a tgz archive

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+#!/user/bin/python3
+"""Creating a tar archive using fab
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Creating the archive
+    """
+    stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz webstatic".formart(stamp))
+        return "versions/web_static_{}.tgz".format(stamp)
+    except Exception as e:
+        return None
