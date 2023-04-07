32aae60dde433763b6c12f19ace0905e4ec93545
Erick <wr.t8r1@gmail.com>
2023-04-06 17:43:51 -0700

    Packing and Deploying

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Deploying the webstatic to the server
+"""
+from fabric.api import local, env, put, run
+from datetime import datetime
+import os
+env.hosts = ["54.152.198.15", "54.234.1.228"]
+
+
+def do_pack():
+    """Creating the archive
+    """
+    stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(stamp))
+        return "versions/web_static_{}.tgz".format(stamp)
+    except Exception as e:
+        return None
+
+
+def do_deploy(archive_path):
+    """Deploy that transfers and extract the tgz file
+    """
+    if not os.path.isfile(archive_path):
+        return False
+
+    file_path = archive_path.split("/")[-1]
+    dir_name = file_path.split(".")[0]
+
+    dir_path = "/data/web_static/releases/{}".format(dir_name)
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(dir_path))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(file_path, dir_path))
+        run("sudo rm /tmp/{}".format(file_path))
+        run("sudo mv {}/web_static/* {}".format(dir_path, dir_path))
+        run("sudo rm -rf {}/web_static".format(dir_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {} /data/web_static/current".format(dir_path))
+        print("New version deployed!")
+    except Exception as e:
+        return False
+
+
+def deploy():
+    """Full deploy of webstatic
+    """
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
